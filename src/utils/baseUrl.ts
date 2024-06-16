c83c181f391cff1ba36b14ce2ccf8aba4de13322
TijanAyo <ayomidetijani6@gmail.com>
2024-06-16 11:14:08 +0100

    Removed staging_url reference from codebase

--- src/utils/baseUrl.ts
+++ src/utils/baseUrl.ts
@@ -1,19 +1,15 @@
 const BASE_URL =
   process.env.NODE_ENV === "production"
     ? String(process.env.PROD_URL)
-    : process.env.NODE_ENV === "staging"
-      ? String(process.env.STAGING_URL)
-      : process.env.NODE_ENV === "local"
-        ? String(process.env.LOCAL_URL)
-        : "";
+    : process.env.NODE_ENV === "local"
+      ? String(process.env.LOCAL_URL)
+      : "";
 
 const SERVER_URL =
   process.env.NODE_ENV === "production"
     ? String(process.env.PROD_SERVER_URL)
-    : process.env.NODE_ENV === "staging"
-      ? String(process.env.STAGING_SERVER_URL)
-      : process.env.NODE_ENV === "local"
-        ? String(process.env.LOCAL_SERVER_URL)
-        : "";
+    : process.env.NODE_ENV === "local"
+      ? String(process.env.LOCAL_SERVER_URL)
+      : "";
 
 export { BASE_URL, SERVER_URL };
