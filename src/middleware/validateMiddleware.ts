57ff90901f7199bb39ca9598c027fc38af3f9a24
TijanAyo <ayomidetijani6@gmail.com>
2024-06-16 11:34:00 +0100

    Added routes and middleware

---
+++ src/middleware/validateMiddleware.ts
@@ -0,0 +1,81 @@
+import { Request, Response, NextFunction } from "express";
+import jwt, { TokenExpiredError } from "jsonwebtoken";
+import User from "../models/User";
+import { BASE_URL, AppResponse } from "../utils";
+import Http from "../constants/statusCodes";
+
+const JWT_SECRET = String(process.env.JWT_SECRET);
+
+export const validateEmailToken = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const token = req.params.token;
+  let decode: jwt.JwtPayload;
+  try {
+    decode = jwt.verify(token, JWT_SECRET) as jwt.JwtPayload;
+
+    const user = await User.findOne({ emailAddress: decode.email });
+    const { isEmailVerified } = user;
+
+    // If clients email is verified redirect client back to login page
+    if (isEmailVerified) {
+      console.log("Email address has been verified initially");
+      return res.redirect(`${BASE_URL}`);
+    }
+
+    next();
+  } catch (err: any) {
+    console.error("ValidateEmailTokenError:", err);
+    if (err instanceof TokenExpiredError) {
+      console.error("Verification token has expired");
+
+      //TODO: This should redirect client to a 'link has expired page' and not a JSON response
+      return AppResponse(
+        res,
+        Http.UNAUTHORIZED,
+        null,
+        "Verification link has expired",
+        false,
+      );
+    }
+
+    return AppResponse(
+      res,
+      Http.INTERNAL_SERVER_ERROR,
+      null,
+      "An error occured while validating token",
+      false,
+    );
+  }
+};
+
+export const authorizeFacility = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    if (req.user.userType !== "Facility") {
+      return AppResponse(
+        res,
+        Http.FORBIDDEN,
+        null,
+        "Facility authorization required",
+        false,
+      );
+    }
+
+    next();
+  } catch (err) {
+    console.error("authorizeFacilityMiddlewareError:", err);
+    return AppResponse(
+      res,
+      Http.UNAUTHORIZED,
+      null,
+      "An unexpected error has occurred",
+      false,
+    );
+  }
+};
