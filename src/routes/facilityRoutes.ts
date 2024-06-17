64340258b1bbdc6a20e090b63e8a35b0e4fc314c
TijanAyo <ayomidetijani6@gmail.com>
2024-06-17 11:43:35 +0100

    Added profileCheck middleware to the facility route to provide extra validation

--- src/routes/facilityRoutes.ts
+++ src/routes/facilityRoutes.ts
@@ -1,16 +1,29 @@
 import express, { Router } from "express";
 import { getRequests, getDonors, requestDonor } from "../controllers";
-import { authMiddleware, authorizeFacility } from "../middleware";
+import { authMiddleware, authorizeFacility, profileCheck } from "../middleware";
 
 const router: Router = express.Router();
 
-router.get("/get-donors", authMiddleware, authorizeFacility, getDonors);
+router.get(
+  "/get-donors",
+  authMiddleware,
+  authorizeFacility,
+  profileCheck,
+  getDonors,
+);
 router.post(
   "/request-donor/:donorId",
   authMiddleware,
   authorizeFacility,
+  profileCheck,
   requestDonor,
 );
-router.get("/get-requests", authMiddleware, authorizeFacility, getRequests);
+router.get(
+  "/get-requests",
+  authMiddleware,
+  authorizeFacility,
+  profileCheck,
+  getRequests,
+);
 
 export { router as facilityRoute };
