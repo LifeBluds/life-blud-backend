07271db7b44bcd522da42ee5287882568d780399
TijanAyo <ayomidetijani6@gmail.com>
2024-06-17 23:07:36 +0100

    Removed PROD_URL and LOCAL_URL

--- src/utils/baseUrl.ts
+++ src/utils/baseUrl.ts
@@ -1,10 +1,3 @@
-const BASE_URL =
-  process.env.NODE_ENV === "production"
-    ? String(process.env.PROD_URL)
-    : process.env.NODE_ENV === "local"
-      ? String(process.env.LOCAL_URL)
-      : "";
-
 const SERVER_URL =
   process.env.NODE_ENV === "production"
     ? String(process.env.PROD_SERVER_URL)
@@ -12,4 +5,4 @@ const SERVER_URL =
       ? String(process.env.LOCAL_SERVER_URL)
       : "";
 
-export { BASE_URL, SERVER_URL };
+export { SERVER_URL };
