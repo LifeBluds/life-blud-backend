8ba4aee2fc4337a2f977bcef1ea49f49d3f7f552
TijanAyo <ayomidetijani6@gmail.com>
2024-06-17 23:14:52 +0100

    Added appointment time when facility is sending a request to a donor

--- src/validation/facilityValidation.ts
+++ src/validation/facilityValidation.ts
@@ -2,6 +2,7 @@ import joi from "joi";
 
 export const requestDonorSchema = joi.object({
   appointmentDate: joi.string().trim().required(),
+  appointmentTime: joi.string().trim().required(),
   additionalInformation: joi.string().trim().lowercase(),
   bloodCollectionType: joi.string().trim().required(),
   bloodGroupRequired: joi.string().trim().required(),
